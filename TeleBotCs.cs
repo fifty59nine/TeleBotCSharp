9cab24c15945d6d9387121f480143e0cd64cc3cd
fifty59nine <csgolife228@gmail.com>
2021-08-06 14:55:13 +0300

    Try to create background service

--- TeleBotCs.cs
+++ TeleBotCs.cs
@@ -24,16 +24,18 @@ namespace CsTeleBot
         private const string BaseApiUrl = "https://api.telegram.org/bot";
         public string Token { private get; set; }
         public string ParseMode { private get; set; }
+        public int Timeout { get; set; }
 
         /// <summary>
         /// CsTeleBot object constructor
         /// </summary>
         /// <param name="Token">Telegram bot api token</param>
         /// <param name="ParseMode">Default parse mode. Details on: https://core.telegram.org/bots/api#formatting-options </param>
-        public TeleBotCs(string Token, string ParseMode = null)
+        public TeleBotCs(string Token, string ParseMode = null, int Timeout = 500)
         {
             this.Token = Token;
             this.ParseMode = ParseMode;
+            this.Timeout = Timeout;
         }
         /// <summary>
         /// A simple method for testing your bot's auth token. Requires no parameters.
@@ -64,107 +66,11 @@ namespace CsTeleBot
             return JsonConvert.DeserializeObject<SendMessageResp>(result);
         }
 
-        private async Task<GetUpdatesResp> GetUpdates()
+        public async Task<GetUpdatesResp> GetUpdates()
         {
             Task<string> taskResult = MakeRequset("getUpdates");
             string result = taskResult.Result;
             return JsonConvert.DeserializeObject<GetUpdatesResp>(result);
-        }
-
-        /// <summary>
-        /// Start check messages
-        /// </summary>
-        /// <param name="timeout">Checking timeout in millisec</param>
-        public async Task Polling(int timeout = 2000)
-        {
-            bool DoesFirstTime = true;
-            List<int> MessagesIds = new List<int>();
-            while (true)
-            {
-                GetUpdatesResp resp = await GetUpdates();
-                foreach (Result result in resp.result)
-                {
-                    if (DoesFirstTime)
-                    {
-                        try
-                        {
-                            MessagesIds.Add(result.message.message_id);
-                        } catch (NullReferenceException)
-                        {
-                            MessagesIds.Add(result.message.message_id);
-                        }
-                    }
-                    try
-                    {
-                        if (MessagesIds.Contains(result.message.message_id)) { }
-                        else
-                        {
-                            Type messageType = null;
-                            if (result.message.photo != null)
-                            {
-                                messageType = typeof(PhotoHandler);
-                            }
-                            else if (result.message.text.StartsWith('/'))
-                            {
-                                messageType = typeof(CommandHandler);
-                            }
-                            else if (result.message.text != null)
-                            {
-                                messageType = typeof(MessageHandler);
-                            }
-                            var methods = AppDomain.CurrentDomain.GetAssemblies()
-                                .SelectMany(x => x.GetTypes())
-                                .Where(x => x.IsClass)
-                                .SelectMany(x => x.GetMethods())
-                                .Where(x => x.GetCustomAttributes(messageType, false).FirstOrDefault() != null);
-                            if (messageType == typeof(CommandHandler))
-                            {
-                                foreach (var x in methods)
-                                {
-                                    foreach (var command in x.GetCustomAttributesData().ToArray().Last().ConstructorArguments)
-                                    {
-                                        if (command.ToString().Replace("\"", "") == result.message.text.Replace("/", ""))
-                                        {
-                                            var temp = Activator.CreateInstance(x.DeclaringType);
-                                            x.Invoke(temp, new object[1] { result });
-                                            break;
-                                        }
-                                    }
-                                }
-                            }
-                            else
-                            {
-                                foreach (var x in methods)
-                                {
-                                    var obj = Activator.CreateInstance(x.DeclaringType);
-                                    x.Invoke(obj, new object[1] { result });
-                                }
-                            }
-                        }
-                        MessagesIds.Add(result.message.message_id);
-                    } catch (NullReferenceException)
-                    {
-                        if (MessagesIds.Contains(result.callback_query.message.message_id)) { }
-                        else
-                        {
-                            var messageType = typeof(CallbackDataHandler);
-                            var methods = AppDomain.CurrentDomain.GetAssemblies()
-                                    .SelectMany(x => x.GetTypes())
-                                    .Where(x => x.IsClass)
-                                    .SelectMany(x => x.GetMethods())
-                                    .Where(x => x.GetCustomAttributes(messageType, false).FirstOrDefault() != null);
-                            foreach (var x in methods)
-                            {
-                                var obj = Activator.CreateInstance(x.DeclaringType);
-                                x.Invoke(obj, new object[1] { result });
-                            }
-                            MessagesIds.Add(result.callback_query.message.message_id);
-                        }                        
-                    }
-                }
-                Thread.Sleep(timeout);
-                DoesFirstTime = false;
-            }
         }
 
         private string httpBuildQuery(Dictionary<string, string> keyValuePairs)
---
+++ TeleBotService.cs
@@ -0,0 +1,125 @@
+﻿using CsTeleBot;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using TeleBotCSharp;
+
+namespace TeleBotCSharp
+{
+    public class TeleBotService : BackgroundService
+    {
+        TeleBotCs bot { get; set; }
+
+        public TeleBotService(TeleBotCs bot)
+        {
+            this.bot = bot;
+        }
+
+
+        public override async Task ExecuteAsync(CancellationToken cancellationToken)
+        {
+            while (!cancellationToken.IsCancellationRequested)
+            {
+                await Polling();
+            }
+        }
+
+        private async Task Polling()
+        {
+            bool DoesFirstTime = true;
+            List<int> MessagesIds = new List<int>();
+            while (true)
+            {
+                GetUpdatesResp resp = await bot.GetUpdates();
+                foreach (Result result in resp.result)
+                {
+                    if (DoesFirstTime)
+                    {
+                        try
+                        {
+                            MessagesIds.Add(result.message.message_id);
+                        }
+                        catch (NullReferenceException)
+                        {
+                            MessagesIds.Add(result.callback_query.message.message_id);
+                        }
+                    }
+                    try
+                    {
+                        if (MessagesIds.Contains(result.message.message_id)) { }
+                        else
+                        {
+                            Type messageType = null;
+                            if (result.message.photo != null)
+                            {
+                                messageType = typeof(PhotoHandler);
+                            }
+                            else if (result.message.text.StartsWith('/'))
+                            {
+                                messageType = typeof(CommandHandler);
+                            }
+                            else if (result.message.text != null)
+                            {
+                                messageType = typeof(MessageHandler);
+                            }
+                            var methods = AppDomain.CurrentDomain.GetAssemblies()
+                                .SelectMany(x => x.GetTypes())
+                                .Where(x => x.IsClass)
+                                .SelectMany(x => x.GetMethods())
+                                .Where(x => x.GetCustomAttributes(messageType, false).FirstOrDefault() != null);
+                            if (messageType == typeof(CommandHandler))
+                            {
+                                foreach (var x in methods)
+                                {
+                                    foreach (var command in x.GetCustomAttributesData().ToArray().Last().ConstructorArguments)
+                                    {
+                                        if (command.ToString().Replace("\"", "") == result.message.text.Replace("/", ""))
+                                        {
+                                            var temp = Activator.CreateInstance(x.DeclaringType);
+                                            x.Invoke(temp, new object[1] { result });
+                                            break;
+                                        }
+                                    }
+                                }
+                            }
+                            else
+                            {
+                                foreach (var x in methods)
+                                {
+                                    var obj = Activator.CreateInstance(x.DeclaringType);
+                                    x.Invoke(obj, new object[1] { result });
+                                }
+                            }
+                        }
+                        MessagesIds.Add(result.message.message_id);
+                    }
+                    catch (NullReferenceException)
+                    {
+                        if (MessagesIds.Contains(result.callback_query.message.message_id)) { }
+                        else
+                        {
+                            var messageType = typeof(CallbackDataHandler);
+                            var methods = AppDomain.CurrentDomain.GetAssemblies()
+                                    .SelectMany(x => x.GetTypes())
+                                    .Where(x => x.IsClass)
+                                    .SelectMany(x => x.GetMethods())
+                                    .Where(x => x.GetCustomAttributes(messageType, false).FirstOrDefault() != null);
+                            foreach (var x in methods)
+                            {
+                                var obj = Activator.CreateInstance(x.DeclaringType);
+                                x.Invoke(obj, new object[1] { result });
+                            }
+                            MessagesIds.Add(result.callback_query.message.message_id);
+                        }
+                    }
+                }
+                Thread.Sleep(this.bot.Timeout);
+                DoesFirstTime = false;
+            }
+        }
+    }
+}
